13780a5fecfff9bb7ad55f094af30b9a2ee2957c
Michael Ni <michael.ni678@gmail.com>
2025-09-13 18:34:38 -0400

    feat: visit attributes function, nodes, convert attributes to nodes function, etc

---
+++ doctored-macros/src/utilities/nodes/mod.rs
@@ -0,0 +1,45 @@
+use proc_macro2::Span;
+use syn::{AttrStyle, Attribute, spanned::Spanned};
+
+pub mod convert;
+
+#[derive(Clone)]
+pub struct Node {
+    pub kind: NodeKind,
+    pub style: AttrStyle,
+}
+
+impl Node {
+    pub fn span(&self) -> Span {
+        match self.kind {
+            NodeKind::Argument(ArgumentNode { span, .. })
+            | NodeKind::Documentation(DocumentationNode { span, .. }) => span,
+            NodeKind::Unrelated(ref attr) => attr.span(),
+        }
+    }
+}
+
+#[derive(Clone)]
+#[allow(clippy::large_enum_variant)]
+pub enum NodeKind {
+    Argument(ArgumentNode),
+    Documentation(DocumentationNode),
+    Unrelated(Attribute),
+}
+
+#[derive(Clone)]
+pub struct ArgumentNode {
+    pub kind: ArgumentKind,
+    pub span: Span,
+}
+
+#[derive(Clone)]
+pub enum ArgumentKind {
+    SummaryHide,
+}
+
+#[derive(Clone)]
+pub struct DocumentationNode {
+    pub string: String,
+    pub span: Span,
+}
