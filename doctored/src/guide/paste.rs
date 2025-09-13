b36592712d0640cf594b87038b48496401d018d4
Michael Ni <michael.ni678@gmail.com>
2025-09-13 19:50:31 -0400

    docs: paste documentation

---
+++ doctored/src/guide/paste.rs
@@ -0,0 +1,6 @@
+//! Pastes documentation.
+//!
+//! The `paste` attribute is used to paste documentation. A tag must be
+//! supplied.
+//!
+//! See the [`copy`](super::copy) attribute for more information.
