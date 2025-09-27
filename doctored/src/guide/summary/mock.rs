005c3b414a30253df874d055cbbab11b65329bb9
Michael Ni <michael.ni678@gmail.com>
2025-09-26 22:38:57 -0400

    docs: unmocked and mocked struct examples

--- doctored/src/guide/summary/mock.rs
+++ doctored/src/guide/summary/mock.rs
@@ -18,3 +18,18 @@ pub mod mocked {
     //!
     //! See [unmocked](super::unmocked) for its counterpart.
 }
+
+/// This summary appears in the documentation!
+///
+/// The text above appears in the module overview, as well as on the actual
+/// documentation.
+///
+/// See [Mocked](super::Mocked) for its counterpart.
+pub struct Unmocked;
+
+#[doc(summary(mock = "This summary doesn't appear in the documentation!"))]
+/// This is the first line, but the summary in the module overview is
+/// different!
+///
+/// See [Unmocked](super::Unmocked) for its counterpart.
+pub struct Mocked;
