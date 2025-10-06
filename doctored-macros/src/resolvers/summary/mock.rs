ac1f5d57f78b396254a32eb7b8792d59c7779a79
Michael Ni <michael.ni678@gmail.com>
2025-10-05 23:55:25 -0400

    refactor: mock add the inline html to the same doc node

--- doctored-macros/src/resolvers/summary/mock.rs
+++ doctored-macros/src/resolvers/summary/mock.rs
@@ -1,4 +1,4 @@
-use indoc::{formatdoc, indoc};
+use indoc::formatdoc;
 use syn::Result;
 
 use crate::utilities::nodes::{ArgumentKind, ArgumentNode, DocumentationNode, Node, NodeKind};
@@ -19,45 +19,29 @@ pub fn resolve_summary_mock(nodes: &mut Vec<Node>) -> Result<()> {
         let span = node.span();
         let style = node.style;
 
-        nodes.splice(
-            0..0,
-            [
-                Node {
-                    kind: NodeKind::Documentation(DocumentationNode {
-                        string: formatdoc! {r#"
+        nodes.insert(
+            0,
+            Node {
+                kind: NodeKind::Documentation(DocumentationNode {
+                    string: formatdoc! {r#"
                             <div id="doctored-summary-mock">
                                 {summary}
                             </div>
-                        "#},
-                        span,
-                    }),
-                    style,
-                },
-                Node {
-                    kind: NodeKind::Documentation(DocumentationNode {
-                        string: String::new(),
-                        span,
-                    }),
-                    style,
-                },
-                Node {
-                    kind: NodeKind::Documentation(DocumentationNode {
-                        string: String::from(indoc! {r#"
+
                             <style>
-                                #doctored-summary-mock {
+                                #doctored-summary-mock {{
                                     display: none;
-                                }
+                                }}
                             </style>
-                        "#}),
-                        span,
-                    }),
-                    style,
-                },
-            ],
+                        "#},
+                    span,
+                }),
+                style,
+            },
         );
 
         // Resolve the node, which is now offset by 3.
-        nodes[index + 3].resolve();
+        nodes[index + 1].resolve();
 
         break;
     }
