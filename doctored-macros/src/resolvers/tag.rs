d572558812cbeac8b2d5ee2a96e8ed5d00660f55
Michael Ni <michael.ni678@gmail.com>
2025-10-06 00:51:19 -0400

    feat: prevent cursor from highlighting tag if there's no hyperlink

--- doctored-macros/src/resolvers/tag.rs
+++ doctored-macros/src/resolvers/tag.rs
@@ -21,57 +21,66 @@ pub fn resolve_tag(nodes: &mut Vec<Node>) -> Result<()> {
         let span = node.span();
         let style = node.style;
 
-        let text = format!(r#"tag.innerText = "{text}";"#);
-
-        let href = href
-            .map(|href| format!(r#"tag.setAttribute("href", "{href}");"#))
-            .unwrap_or_default();
-
         let color = color.unwrap_or_else(|| String::from(DEFAULT_TAG_COLOR));
 
+        let mut html_script = String::new();
+
+        html_script.extend([
+            formatdoc! {r#"
+                const heading = document.body.querySelector(".main-heading h1");
+
+                const tagContainer = document.createElement("div");
+                tagContainer.className = "doctored-tag-container";
+
+                heading.appendChild(tagContainer);
+            "#},
+            // If a hyperlink is provided, use an anchor. Otherwise, use a span so the text doesn't
+            // get underlined when the cursor hovers over it.
+            if let Some(href) = href {
+                formatdoc! {r#"
+                    const tag = document.createElement("a");
+                    tag.setAttribute("href", "{href}");
+                "#}
+            } else {
+                format!(r#"const tag = document.createElement("span");"#)
+            },
+            formatdoc! {r#"
+                tag.innerText = "{text}";
+                tag.className = "doctored-tag";
+
+                tagContainer.appendChild(tag);
+            "#},
+        ]);
+
+        let html_style = formatdoc! {r#"
+            .doctored-tag-container {{
+                padding: 0.5rem 0;
+                display: flex;
+                flex-wrap: wrap;
+                gap: 0.5rem;
+            }}
+            
+            .doctored-tag {{
+                display: flex;
+                align-items: center;
+                width: fit-content;
+                height: 1.5rem;
+                padding: 0 0.5rem;
+                border-radius: 0.75rem;
+                font-size: 1rem;
+                font-weight: normal;
+                color: white;
+                background-color: {color};
+            }}
+        "#};
+
+        let string = formatdoc! {r#"
+            <script>{html_script}</script>
+            <style>{html_style}</style>
+        "#};
+
         nodes.push(Node {
-            kind: NodeKind::Documentation(DocumentationNode {
-                string: formatdoc! {r#"
-                        <script>
-                            const heading = document.body.querySelector(".main-heading h1");
-
-                            const tagContainer = document.createElement("div");
-                            tagContainer.className = "doctored-tag-container";
-
-                            heading.appendChild(tagContainer);
-                            
-                            const tag = document.createElement("a");
-                            {text}
-                            {href}
-                            tag.className = "doctored-tag";
-
-                            tagContainer.appendChild(tag);
-                        </script>
-
-                        <style>
-                            .doctored-tag-container {{
-                                padding: 0.5rem 0;
-                                display: flex;
-                                flex-wrap: wrap;
-                                gap: 0.5rem;
-                            }}
-                            
-                            .doctored-tag {{
-                                display: flex;
-                                align-items: center;
-                                width: fit-content;
-                                height: 1.5rem;
-                                padding: 0 0.5rem;
-                                border-radius: 0.75rem;
-                                font-size: 1rem;
-                                font-weight: normal;
-                                color: white;
-                                background-color: {color};
-                            }}
-                        </style>
-                    "#},
-                span,
-            }),
+            kind: NodeKind::Documentation(DocumentationNode { string, span }),
             style,
         });
 
