250b4f4809c39468e40f2e1a1428b9ec2e773b0a
Michael Ni <michael.ni678@gmail.com>
2025-10-06 12:25:37 -0400

    style: remove trailing comma

--- doctored/src/guide/tag.rs
+++ doctored/src/guide/tag.rs
@@ -2,7 +2,7 @@
 
 //! Categorize items.
 
-#[doc(tag(text = "Struct", color = "#44ad67ff",))]
+#[doc(tag(text = "Struct", color = "#44ad67ff"))]
 /// This struct is tagged.
 ///
 /// See [Untagged] for its counterpart.
@@ -14,7 +14,7 @@ pub struct Tagged;
 pub struct Untagged;
 
 pub mod tagged {
-    #![doc(tag(text = "Module", href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ",))]
+    #![doc(tag(text = "Module", href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"))]
     //! This module is tagged with a hyperlink.
     //!
     //! See [untagged](super::untagged) for its counterpart.
