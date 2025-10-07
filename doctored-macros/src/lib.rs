3de3c67f5513c8a637575b39d2d064d964c1294c
Michael Ni <michael.ni678@gmail.com>
2025-10-06 22:59:18 -0400

    refactor: remove the resolve function so it matches the style of the parsers

--- doctored-macros/src/lib.rs
+++ doctored-macros/src/lib.rs
@@ -1,8 +1,15 @@
+use std::collections::HashMap;
+
 use proc_macro::TokenStream;
 use syn::{Error, parse::Nothing, parse_macro_input};
 
 use crate::{
-    resolvers::resolve,
+    resolvers::{
+        clipboard::{copy::resolve_clipboard_copy, paste::resolve_clipboard_paste},
+        highlight::resolve_highlight,
+        summary::resolve_summary,
+        tag::resolve_tag,
+    },
     utilities::{
         attributes::visit::visit_attributes,
         nodes::convert::{convert_attributes_into_nodes, convert_nodes_into_attributes},
@@ -20,7 +27,16 @@ pub fn doctored(args: TokenStream, input: TokenStream) -> TokenStream {
     visit_attributes(input.into(), &mut |attrs| {
         let mut nodes = convert_attributes_into_nodes(attrs)?;
 
-        resolve(&mut nodes)?;
+        let mut clipboard = HashMap::new();
+
+        resolve_clipboard_copy(&mut nodes, &mut clipboard)?;
+        resolve_clipboard_paste(&mut nodes, clipboard)?;
+
+        resolve_highlight(&mut nodes)?;
+
+        resolve_summary(&mut nodes)?;
+
+        resolve_tag(&mut nodes)?;
 
         convert_nodes_into_attributes(nodes)
     })
