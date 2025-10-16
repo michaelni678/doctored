b4bd6f13f72ea746a6020474785f6a450135e837
Michael Ni <michael.ni678@gmail.com>
2025-10-16 17:20:56 -0400

    docs: summary hide docs

--- doctored/src/guide/summary/hide.rs
+++ doctored/src/guide/summary/hide.rs
@@ -1,37 +1,23 @@
 #![cfg_attr(feature = "guide", doctored::doctored)]
 
 //! Hides the summary in the module overview.
+//! 
+//! # Example
+#![doc(highlight)]
+//! ```
+#![doc(extras(include(documentation = "doctored/src/guide/summary/hide.Summarized.example")))]
+//! pub struct Summarized;
+//! 
+#![doc(extras(include(documentation = "doctored/src/guide/summary/hide.NotSummarized.example")))]
+//! pub struct NotSummarized;
+//! ```
+//! 
+//! # Expansion
+//!
+//! See [`Summarized`] and [`NotSummarized`].
 
-pub mod shown {
-    //! This summary is shown!
-    //!
-    //! The text above appears in the module overview.
-    //!
-    //! See [hidden](super::hidden) for its counterpart.
-}
+#[doc(extras(include(attributes = "doctored/src/guide/summary/hide.Summarized.example")))]
+pub struct Summarized;
 
-pub mod hidden {
-    #![doc(summary(hide))]
-    //! This summary is hidden!
-    //!
-    //! Even though the text above is on the first line, it does not appear in
-    //! the module overview.
-    //!
-    //! See [shown](super::shown) for its counterpart.
-}
-
-/// This summary is shown!
-///
-/// The text above appears in the module overview.
-///
-/// See [`Hidden`] for its counterpart.
-pub struct Shown;
-
-#[doc(summary(hide))]
-/// This summary is hidden!
-///
-/// Even though the text above is on the first line, it does not appear in the
-/// module overview.
-///
-/// See [`Shown`] for its counterpart.
-pub struct Hidden;
+#[doc(extras(include(attributes = "doctored/src/guide/summary/hide.NotSummarized.example")))]
+pub struct NotSummarized;
