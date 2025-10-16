61b7b45f73853315371e6fdfee2f10d1acee6535
Michael Ni <michael.ni678@gmail.com>
2025-10-16 17:31:33 -0400

    docs: mock documentation

--- doctored/src/guide/summary/hide.rs
+++ doctored/src/guide/summary/hide.rs
@@ -1,17 +1,19 @@
 #![cfg_attr(feature = "guide", doctored::doctored)]
 
+#![rustfmt::skip]
+
 //! Hides the summary in the module overview.
-//! 
+//!
 //! # Example
 #![doc(highlight)]
 //! ```
 #![doc(extras(include(documentation = "doctored/src/guide/summary/hide.Summarized.example")))]
 //! pub struct Summarized;
-//! 
+//!
 #![doc(extras(include(documentation = "doctored/src/guide/summary/hide.NotSummarized.example")))]
 //! pub struct NotSummarized;
 //! ```
-//! 
+//!
 //! # Expansion
 //!
 //! See [`Summarized`] and [`NotSummarized`].
