ba79b0d471d486650881e499fa55605f09f042e7
Michael Ni <michael.ni678@gmail.com>
2025-10-15 23:06:11 -0400

    docs: root documentation

--- doctored/src/lib.rs
+++ doctored/src/lib.rs
@@ -1,8 +1,46 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(feature = "guide", feature(doc_auto_cfg))]
 #![cfg_attr(
-    any(feature = "documentation", docsrs),
+    any(feature = "guide"),
     feature(custom_inner_attributes, proc_macro_hygiene)
 )]
+
+//! Rust documentation utilities.
+//!
+//! # Setup
+//!
+//! Add Doctored to your Cargo.toml.
+//!
+//! ```toml
+//! [dependencies]
+//! doctored = "0.1.0"
+//! ```
+//!
+//! Add a feature to your Cargo.toml. Unfortunately, you'll need to enable this
+//! feature whenever Doctored's macros should be expanded.
+//!
+//! ```toml
+//! [features]
+//! nightly = []
+//!
+//! [package.metadata.docs.rs]
+//! features = ["nightly"]
+//! ```
+//!
+//! At the top of the crate root, enable the nightly `custom_inner_attributes`
+//! and `proc_macro_hygiene` features.
+//!
+//! ```text
+//! #![cfg_attr(
+//!     feature = "nightly",
+//!     feature(custom_inner_attributes, proc_macro_hygiene)
+//! )]
+//! ```
+//! 
+//! Add the snippet below to the top of modules that use Doctored.
+//! 
+//! ```text
+//! #![cfg_attr(feature = "guide", doctored::doctored)]
+//! ```
 
 extern crate self as doctored;
 
