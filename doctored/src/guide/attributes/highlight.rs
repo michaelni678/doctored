8508099be9ab529963db5f306e3e7081ad7db2dd
Michael Ni <michael.ni678@gmail.com>
2025-10-18 23:59:21 -0400

    docs: highlight syntax docs

--- doctored/src/guide/attributes/highlight.rs
+++ doctored/src/guide/attributes/highlight.rs
@@ -6,10 +6,28 @@
 //! blocks. When the code block is doctested, the test will always pass,
 //! regardless of whether the code can compile.
 //!
-//! This behaves similarly to Rust's built-in [`ignore` attribute](https://doc.rust-lang.org/rustdoc/write-documentation/documentation-tests.html#attributes).
+//! This behaves similarly to Rustdoc's built-in [`ignore` attribute](https://doc.rust-lang.org/rustdoc/write-documentation/documentation-tests.html#attributes).
 //! The key difference is that doctests marked with `ignore` display a tooltip
 //! stating that the code block was not tested, whereas those generated with
-//! `highlight` do not.
+//! Doctored's `highlight` do not.
+//!
+//! Doctored scans documentation for code blocks marked with the `highlight`
+//! attribute.
+#![doc(highlight)]
+//! ```
+//! //! ```highlight
+//! //! pub fn foo() {}
+//! //! ```
+//! ```
+//!
+//! You can also use an actual attribute placed directly above a code block.
+#![doc(highlight)]
+//! ```
+//! #![doc(highlight)]
+//! //! ```
+//! //! pub fn foo() {}
+//! //! ```
+//! ```
 //!
 //! # Example
 #![doc(highlight)]
