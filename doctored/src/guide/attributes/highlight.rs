f36479fa2d5ebbc92ba9b360143ec82a4ed9ddb5
Michael Ni <michael.ni678@gmail.com>
2025-10-23 22:26:23 -0400

    docs: highlight example

---
+++ doctored/src/guide/attributes/highlight.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(feature = "guide", doctored::doctored)]
+
+//! Syntax-highlight code blocks.
+//!
+//! # Example
+//!
+#![doc(disregard)]
+//! ```
+#![doc(extras(include(documentation = "src/guide/attributes/highlight.example")))]
+//! ```
+//!
+//! # Expansion
+//!
+#![doc(extras(include(attributes = "src/guide/attributes/highlight.example")))]
