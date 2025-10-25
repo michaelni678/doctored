259568cc2cab8d7ddd23d4256f84722b34c5cc29
Michael Ni <michael.ni678@gmail.com>
2025-10-24 21:19:18 -0400

    docs: theme explanation

--- doctored/src/guide/attributes/highlight.rs
+++ doctored/src/guide/attributes/highlight.rs
@@ -7,6 +7,13 @@
 //! Doctored uses [highlight.js](https://github.com/highlightjs/highlight.js)
 //! under the hood.
 //!
+//! # Theme
+//!
+//! Syntax highlighting uses a theme imitating after Rustdoc. Many
+//! [HLJS scopes](https://highlightjs.readthedocs.io/en/latest/css-classes-reference.html)
+//! are still unstyled, so if anything looks off, please
+//! [open an issue](https://github.com/michaelni678/doctored/issues?q=sort%3Aupdated-desc+is%3Aissue+is%3Aopen).
+//!
 //! # Example
 //!
 #![doc(disregard)]
